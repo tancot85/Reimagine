6eb8c22179e308cc8b38258d59bdf8f54dcc7ab8
Tanmay <tanmay.tete@somaiya.edu>
2021-11-24 15:33:29 +0530

    changed bot credentials

--- ReImagine/app/urls.py
+++ ReImagine/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('', views.content_page, name='homepage'),
 ]
--- ReImagine/app/views.py
+++ ReImagine/app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import ine
+# from models import Post
+from . import ine_component
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def homepage(request):
-    return render(request, 'content.html')
+
+def content_page(request):
+    posts = ine_component.get_posts('mumbai',10)
+    return render(request, 'content.html', {'name_of_subreddit': 'mumbai', 'posts': posts})
