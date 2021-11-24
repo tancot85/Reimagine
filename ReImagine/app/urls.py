aa4fb17ec201c1e715fa37c3dc68f662f084a152
Tanmay <tanmay.tete@somaiya.edu>
2021-11-24 23:25:39 +0530

    kuch to change hua

--- ReImagine/app/urls.py
+++ ReImagine/app/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.content_page, name='homepage'),
-    path('content',views.content_page,name = 'content')
+    path('content',views.content_page,name = 'content'),
+    # path('download_button',views.download_button,name = 'download_button'),
+
 ]
--- ReImagine/app/views.py
+++ ReImagine/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, response
 from . import ine
 from . import ine_component
+import wget
 # Create your views here.
 
 
@@ -16,3 +17,15 @@ def content_page(request):
         query = 'ImaginaryLandscapes'
     posts = ine_component.get_posts(query,10)
     return render(request, 'content.html', {'name_of_subreddit': query, 'posts': posts})
+
+# def download_button(request):
+#     q = request.GET.get()
+#     print(q)
+#     if(request.GET.GET('moredata')):
+#         wget.download(request.GET.get('moredata'))
+#     if(request.GET.get('title')):
+#         query = request.GET.get('title')
+#     else:
+#         query = 'ImaginaryLandscapes'
+#     posts = ine_component.get_posts(query,10)
+#     return render(request, 'content.html', {'name_of_subreddit': query, 'posts': posts})
