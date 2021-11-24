36cd37c0253fee0481ec65a0041a9baa8ca3617f
Tanmay <tanmay.tete@somaiya.edu>
2021-11-24 22:10:44 +0530

    ekdam bada commit HUGE

--- ReImagine/app/urls.py
+++ ReImagine/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.content_page, name='homepage'),
+    path('content',views.content_page,name = 'content')
 ]
--- ReImagine/app/views.py
+++ ReImagine/app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 from . import ine
-# from models import Post
 from . import ine_component
 # Create your views here.
 
@@ -11,5 +10,9 @@ def index(request):
 
 
 def content_page(request):
-    posts = ine_component.get_posts('ImaginaryDemons',10)
-    return render(request, 'content.html', {'name_of_subreddit': 'ImaginaryDemons', 'posts': posts})
+    if(request.GET.get('data')):
+        query = request.GET.get('data')
+    else:
+        query = 'ImaginaryLandscapes'
+    posts = ine_component.get_posts(query,10)
+    return render(request, 'content.html', {'name_of_subreddit': query, 'posts': posts})
