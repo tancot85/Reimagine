184ce9e89c949b907fb52e2b71cd731252b34c0e
LuvArora-coder <luv.a@somaiya.edu>
2021-11-29 03:17:31 +0530

    All Logins Done

--- ReImagine/app/urls.py
+++ ReImagine/app/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.content_page, name='homepage'),
-    path('content',views.content_page,name = 'content'),
+    path('content', views.content_page, name='content'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
     # path('download_button',views.download_button,name = 'download_button'),
 
 ]
--- ReImagine/app/views.py
+++ ReImagine/app/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, response
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib import messages
+from .forms import SignUpForm, EditProfileForm
 from . import ine
 from . import ine_component
-import wget
+#import wget
 # Create your views here.
 
 
@@ -15,8 +19,80 @@ def content_page(request):
         query = request.GET.get('data')
     else:
         query = 'ImaginaryLandscapes'
-    posts = ine_component.get_posts(query,10)
+    posts = ine_component.get_posts(query, 10)
     return render(request, 'content.html', {'name_of_subreddit': query, 'posts': posts})
+
+
+def login_user(request):
+    if request.method == 'POST':  # if someone fills out form , Post it
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:  # if user exist
+            login(request, user)
+            messages.success(request, ('Youre logged in'))
+            # routes to 'content on successful login
+            return redirect('content')
+        else:
+            messages.success(request, ('Error logging in'))
+            # re routes to login page upon unsucessful login
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('Youre now logged out'))
+    return redirect('content')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Youre now registered'))
+            return redirect('content')
+    else:
+        form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'authenticate/register.html', context)
+
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('You have edited your profile'))
+            return redirect('content')
+    else:  # passes in user information
+        form = EditProfileForm(instance=request.user)
+
+    context = {'form': form}
+    return render(request, 'authenticate/edit_profile.html', context)
+    # return render(request, 'authenticate/edit_profile.html',{})
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, ('You have edited your password'))
+            return redirect('content')
+    else:  # passes in user information
+        form = PasswordChangeForm(user=request.user)
+
+    context = {'form': form}
+    return render(request, 'authenticate/change_password.html', context)
 
 # def download_button(request):
 #     q = request.GET.get()
